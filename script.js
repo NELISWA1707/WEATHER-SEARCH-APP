bba8eaf5c1dad8f5b88a635d42c3ad1dcee455fe
NELISWA1707 <mthembuneliswa9@gmail.com>
2024-12-09 10:35:06 +0200

    update on CSS link

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ function search(event) {
   let searchInputElement = document.querySelector("#search-input");
   let city = searchInputElement.value;
 
-  let apiKey = "69486f3703b7o03b7a5448ae33t4c4f7";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  let apiKey = "69486f3703b7o03b7a5448ae33t4c4f7ss";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
 
   axios.get(apiUrl).then(displayTemperature);
 }
